7eeb908c56c78acdd4233f9b8fd316534e1e35d7
Paul Albee <palbee@vulcan.com>
2021-01-13 20:22:21 -0800

    Basic accordion panel.

--- src/main/java/com/vulcan/vmlci/AccordionPanel.java
+++ src/main/java/com/vulcan/vmlci/AccordionPanel.java
@@ -2,36 +2,32 @@ package com.vulcan.vmlci;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 
 public class AccordionPanel extends JPanel {
-    private Icon left_arrow = null;
-    private Icon down_arrow = null;
-    private JPanel title_panel;
     private JPanel content_panel;
     private JCheckBox controlButton;
 
     /**
-     * Creates a new <code>AccordianPanel</code>
+     * Creates a new <code>AccordionPanel</code>
      */
     public AccordionPanel(String title, boolean start_open) {
         this.build_ui(title, start_open);
     }
 
     private void build_ui(String title, boolean initial_state) {
-        left_arrow = new ImageIcon(AccordionPanel.class.getResource("/images/arrow_left.png"));
-        down_arrow = new ImageIcon(AccordionPanel.class.getResource("/images/arrow_down.png"));
+        Icon left_arrow = new ImageIcon(AccordionPanel.class.getResource("/images/arrow_left.png"));
+        Icon down_arrow = new ImageIcon(AccordionPanel.class.getResource("/images/arrow_down.png"));
         controlButton = new JCheckBox(left_arrow);
         controlButton.setSelectedIcon(down_arrow);
         controlButton.setSelected(initial_state);
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        this.title_panel = new JPanel(new BorderLayout());
-        this.title_panel.add(new JLabel(title), BorderLayout.CENTER);
-        this.title_panel.add(controlButton, BorderLayout.EAST);
+        JPanel title_panel = new JPanel(new BorderLayout());
+        title_panel.add(new JLabel(title), BorderLayout.CENTER);
+        title_panel.add(controlButton, BorderLayout.EAST);
 
-        this.add(this.title_panel);
+        this.add(title_panel);
 
         this.content_panel = new JPanel();
         this.content_panel.setVisible(true);
@@ -39,12 +35,9 @@ public class AccordionPanel extends JPanel {
 
 
         // Register event handler for button
-        controlButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                content_panel.setVisible(controlButton.isSelected());
-                content_panel.invalidate();
-            }
+        controlButton.addActionListener(e -> {
+            content_panel.setVisible(controlButton.isSelected());
+            content_panel.invalidate();
         });
     }
 
@@ -59,7 +52,7 @@ public class AccordionPanel extends JPanel {
 
     /**
      * Replaces the current content panel
-     * @param content_panel
+     * @param content_panel - A replacement JPanel instance.
      */
     public void setContent_panel(JPanel content_panel) {
         this.content_panel = content_panel;
