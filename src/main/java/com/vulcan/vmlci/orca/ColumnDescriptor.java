671b9dfbe127afc2112ee4f401ba14891c1187aa
Paul Albee <palbee@vulcan.com>
2021-01-18 23:11:42 -0800

    Data class for holding column descriptors.

---
+++ src/main/java/com/vulcan/vmlci/orca/ColumnDescriptor.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright Vulcan Inc. 2021
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.vulcan.vmlci.orca;
+
+public class ColumnDescriptor {
+    public final String description;
+    public final String units;
+    public final boolean export;
+    public final String measurement_type;
+    public final boolean editable;
+    public final boolean is_metadata;
+    public final int index;
+
+    public ColumnDescriptor(String description, String units, String export, String measurement_type, String editable, String is_metadata, int index) {
+        this.description = description;
+        this.units = units;
+        this.export = export.equalsIgnoreCase("true");
+        this.measurement_type = measurement_type;
+        this.editable = editable.equalsIgnoreCase("true");
+        this.is_metadata = is_metadata.equalsIgnoreCase("true");
+        this.index = index;
+    }
+}
