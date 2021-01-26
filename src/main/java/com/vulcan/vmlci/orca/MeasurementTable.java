8ad711a1f40e2fd5fdcee905ca03cac71146ac26
Paul Albee <palbee@vulcan.com>
2021-01-25 22:35:02 -0800

    Implemented the MeasurementTable window

---
+++ src/main/java/com/vulcan/vmlci/orca/MeasurementTable.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright Vulcan Inc. 2021
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.vulcan.vmlci.orca;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import java.awt.Color;
+import java.awt.event.WindowAdapter;
+
+public class MeasurementTable extends WindowAdapter implements TableModelListener {
+  private DataStore ds;
+  private JFrame frame;
+
+  public MeasurementTable(DataStore ds) {
+    this.ds = ds;
+    build_ui();
+    this.ds.addTableModelListener(this);
+  }
+
+  private void build_ui() {
+    JTable table = new JTable(ds);
+    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+    table.setShowGrid(true);
+    table.setGridColor(Color.BLACK);
+    table.doLayout();
+    JScrollPane scrollPane = new JScrollPane(table);
+    frame = new JFrame(ds.getCsvFileName());
+    frame.add(scrollPane);
+    frame.pack();
+  }
+
+  final public JFrame getFrame() {
+    return frame;
+  }
+
+  /**
+   * This fine grain notification tells listeners the exact range of cells, rows, or columns that
+   * changed.
+   *
+   * @param e
+   */
+  @Override
+  public void tableChanged(TableModelEvent e) {
+    if (ds.dirty()) {
+      frame.setTitle(String.format("%s - Unsaved Changes", ds.getCsvFileName()));
+    } else {
+      frame.setTitle(ds.getCsvFileName());
+    }
+  }
+}
