33138c9ebaa6b7d7db5506cce165c1a05bd326d7
Paul Albee <palbee@vulcan.com>
2021-01-25 22:35:56 -0800

    Added DataStore and fixed formatting.

--- src/main/java/com/vulcan/vmlci/orca/ControlWindow.java
+++ src/main/java/com/vulcan/vmlci/orca/ControlWindow.java
@@ -31,132 +31,142 @@
 
 package com.vulcan.vmlci.orca;
 
-
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 /**
  * The <code>ControlWindow</code> class is the main UI for the Aquatic Mammal Photogrammetry Tool.
  */
 public class ControlWindow {
-    private JFrame application_frame = null;
-    private JPanel toplevel = null;
-    private JPanel branding = null;
-    private final JPanel metadata = null;
-    private final JPanel input = null;
-    private final JPanel length_measurements = null;
-    private final JPanel body_profiles = null;
-    private JPanel csv_controls = null;
+  private final JPanel metadata = null;
+  private final JPanel input = null;
+  private final JPanel length_measurements = null;
+  private final JPanel body_profiles = null;
+  private JFrame application_frame = null;
+  private JPanel toplevel = null;
+  private JPanel branding = null;
+  private JPanel csv_controls = null;
+  private DataStore ds;
 
-
-    private JComponent build_accordion() {
-        JPanel frame = new JPanel();
-        JScrollPane scrollPane = new JScrollPane(frame);
-        GridBagConstraints gbc = new GridBagConstraints();
-        frame.setLayout(new GridBagLayout());
-
-
-        gbc.anchor = GridBagConstraints.NORTH;
-        gbc.weightx = 1;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weighty = 0;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        AccordionPanel demo1 = new AccordionPanel("Metadata", true);
-        demo1.getContent_panel().add(new JLabel("Lorem Ipsum"));
-        frame.add(demo1, gbc);
-
-        gbc.anchor = GridBagConstraints.NORTH;
-        gbc.weighty = 0;
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        AccordionPanel demo2 = new AccordionPanel("Input", true);
-        demo2.getContent_panel().add(new JLabel("Lorem Ipsum"));
-        frame.add(demo2, gbc);
-
-        gbc.anchor = GridBagConstraints.NORTH;
-        gbc.weighty = 0;
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        AccordionPanel demo3 = new AccordionPanel("Length Measurements", true);
-        demo3.getContent_panel().add(new JLabel("Lorem Ipsum"));
-        frame.add(demo3, gbc);
-
-        gbc.anchor = GridBagConstraints.NORTH;
-        gbc.weighty = 0;
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        AccordionPanel demo4 = new AccordionPanel("Body Profiles", true);
-        demo4.getContent_panel().add(new JLabel("Lorem Ipsum"));
-        frame.add(demo4, gbc);
-
-        final JPanel spacer = new JPanel();
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        gbc.weighty = 1;
-        gbc.fill = GridBagConstraints.VERTICAL;
-        frame.add(spacer, gbc);
-        frame.setBackground(Color.CYAN);
-        return scrollPane;
+  public ControlWindow() {
+    try {
+      ds = new DataStore();
+    } catch (ConfigurationFileLoadException | DataFileLoadException e) {
+      e.printStackTrace();
     }
 
-    private void build_ui() {
-        GridBagConstraints c = new GridBagConstraints();
-        this.toplevel = new JPanel();
-        this.toplevel.setLayout(new GridBagLayout());
-
-        GridBagConstraints gbc;
-
-        // Constraints for branding
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.gridwidth = 1;
-        gbc.weightx = 1.0;
-        gbc.anchor = GridBagConstraints.NORTH;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-
-
-        // Branding
-        this.branding = new Branding();
-        this.toplevel.add(branding, gbc);
-
-        // Center Space
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.gridwidth = 1;
-        gbc.gridheight = 1;
-        gbc.weighty = 1.0;
-        gbc.fill = GridBagConstraints.BOTH;
-        this.toplevel.add(build_accordion(), gbc);
-
-        // Data Controls
-        this.csv_controls = new DataControls();
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridwidth = 1;
-        gbc.weightx = 1.0;
-        gbc.anchor = GridBagConstraints.SOUTH;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        this.toplevel.add(csv_controls, gbc);
-
-        this.application_frame = new JFrame();
-        this.application_frame.setTitle("Test Window");
-        this.application_frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.application_frame.add(toplevel);
-        this.application_frame.pack();
-        this.application_frame.setVisible(true);
-    }
-
-    public ControlWindow() {
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                build_ui();
-            }
+    SwingUtilities.invokeLater(
+        new Runnable() {
+          @Override
+          public void run() {
+            build_ui();
+          }
         });
-    }
+  }
 
+  private void build_ui() {
+    GridBagConstraints c = new GridBagConstraints();
+    this.toplevel = new JPanel();
+    this.toplevel.setLayout(new GridBagLayout());
+
+    GridBagConstraints gbc;
+
+    // Constraints for branding
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.gridwidth = 1;
+    gbc.weightx = 1.0;
+    gbc.anchor = GridBagConstraints.NORTH;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+
+    // Branding
+    this.branding = new Branding();
+    this.toplevel.add(branding, gbc);
+
+    // Center Space
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    gbc.gridwidth = 1;
+    gbc.gridheight = 1;
+    gbc.weighty = 1.0;
+    gbc.fill = GridBagConstraints.BOTH;
+    this.toplevel.add(build_accordion(), gbc);
+
+    // Data Controls
+    this.csv_controls = new DataControls(ds);
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 2;
+    gbc.gridwidth = 1;
+    gbc.weightx = 1.0;
+    gbc.anchor = GridBagConstraints.SOUTH;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    this.toplevel.add(csv_controls, gbc);
+
+    this.application_frame = new JFrame();
+    this.application_frame.setTitle("Test Window");
+    this.application_frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    this.application_frame.add(toplevel);
+    this.application_frame.pack();
+    this.application_frame.setVisible(true);
+  }
+
+  private JComponent build_accordion() {
+    JPanel frame = new JPanel();
+    JScrollPane scrollPane = new JScrollPane(frame);
+    GridBagConstraints gbc = new GridBagConstraints();
+    frame.setLayout(new GridBagLayout());
+
+    gbc.anchor = GridBagConstraints.NORTH;
+    gbc.weightx = 1;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    gbc.weighty = 0;
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    AccordionPanel demo1 = new AccordionPanel("Metadata", true);
+    demo1.getContent_panel().add(new JLabel("Lorem Ipsum"));
+    frame.add(demo1, gbc);
+
+    gbc.anchor = GridBagConstraints.NORTH;
+    gbc.weighty = 0;
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    AccordionPanel demo2 = new AccordionPanel("Input", true);
+    demo2.getContent_panel().add(new JLabel("Lorem Ipsum"));
+    frame.add(demo2, gbc);
+
+    gbc.anchor = GridBagConstraints.NORTH;
+    gbc.weighty = 0;
+    gbc.gridx = 0;
+    gbc.gridy = 2;
+    AccordionPanel demo3 = new AccordionPanel("Length Measurements", true);
+    demo3.getContent_panel().add(new JLabel("Lorem Ipsum"));
+    frame.add(demo3, gbc);
+
+    gbc.anchor = GridBagConstraints.NORTH;
+    gbc.weighty = 0;
+    gbc.gridx = 0;
+    gbc.gridy = 3;
+    AccordionPanel demo4 = new AccordionPanel("Body Profiles", true);
+    demo4.getContent_panel().add(new JLabel("Lorem Ipsum"));
+    frame.add(demo4, gbc);
+
+    final JPanel spacer = new JPanel();
+    gbc.gridx = 0;
+    gbc.gridy = 4;
+    gbc.weighty = 1;
+    gbc.fill = GridBagConstraints.VERTICAL;
+    frame.add(spacer, gbc);
+    frame.setBackground(Color.CYAN);
+    return scrollPane;
+  }
 }
