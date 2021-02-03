f7b2bf61b4c7d0340ca50ac966c098d6740596c1
Paul Albee <palbee@vulcan.com>
2021-02-03 15:52:48 -0800

    Completed implementation of MeasurementManager

--- src/main/java/com/vulcan/vmlci/orca/calculator/MeasurementManager.java
+++ src/main/java/com/vulcan/vmlci/orca/calculator/MeasurementManager.java
@@ -33,24 +33,69 @@ package com.vulcan.vmlci.orca.calculator;
 
 import com.vulcan.vmlci.orca.DataStore;
 
-import java.lang.invoke.MethodHandle;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
-public class MeasurementManager extends BaseCalculator {
-  protected static String configuration_file = null;
-
-  public MeasurementManager(DataStore ds) {
+/** Calculator targeted at updating the derived measurements in the <code>DataStore</code> */
+public class MeasurementManager extends BaseCalculator implements TableModelListener {
+  /**
+   * @param ds the <code>DataStore</code> that the Calculator will operate on.
+   * @throws FileNotFoundException
+   */
+  public MeasurementManager(DataStore ds) throws FileNotFoundException {
     super(ds);
+    ds.addTableModelListener(this);
   }
 
-  public static void main(String[] args) {
-    MeasurementManager instance = new MeasurementManager(null);
-    System.out.println(instance.measurement_funcs);
-    MethodHandle mh = instance.measurement_funcs.get("length");
-    try {
-      Double result = (Double) mh.invokeWithArguments((Object[]) new Double[] {0.0, 0.0, 1.0, 1.0});
-      System.out.println(result);
-    } catch (Throwable throwable) {
-      throwable.printStackTrace();
+  /** @return The name of the configuration file required for this calculator. */
+  @Override
+  protected String getConfigurationFile() {
+    return "MeasurementConf.json";
+  }
+
+  /**
+   * Respond to changes to the data_store.
+   *
+   * <p>This only updates the column and row(s) if a single column has been updated. If more than
+   * one column has been updated, this is indicative of a file load and consequently we don't need
+   * ot do any updates.
+   *
+   * @param event
+   */
+  @Override
+  public void tableChanged(TableModelEvent event) {
+    int column = event.getColumn();
+    if (column != TableModelEvent.ALL_COLUMNS) {
+      String column_name = dataStore.getColumnName(column);
+      if (!column_name.equals("Filename")) {
+        for (int row = event.getFirstRow(); row <= event.getLastRow(); row++) {
+          String row_name = dataStore.getRowName(row);
+          update(row_name, column_name);
+        }
+      }
+    }
+  }
+
+  /**
+   * Updates a measurement if data exists in the columns that contribute to it.
+   *
+   * <p>This method essentially does a breadth first traversal of the effect tree defined implicitly
+   * in <code>BaseCalculator.possible_measurements</code>.
+   *
+   * @param title the name of the row being updated
+   * @param column_base the base name of the column being updated.
+   */
+  private void update(String title, String column_base) {
+    LinkedList<String> available_measurement =
+        new LinkedList<>(possible_measurements.getOrDefault(column_base, new ArrayList<>()));
+    while (available_measurement.size() > 0) {
+      String measure = available_measurement.removeFirst();
+      Double measurement_result = do_measurement(measure, title);
+      dataStore.insert_value(title, measure, measurement_result);
+      available_measurement.addAll(possible_measurements.getOrDefault(measure, new ArrayList<>()));
     }
   }
 }
