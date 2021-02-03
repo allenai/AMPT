439855c14a49ab9e8cb4ade9af536b5081cb8823
Paul Albee <palbee@vulcan.com>
2021-02-02 22:28:16 -0800

    Add class to hold groups of calculation configurations for each calculator class.

---
+++ src/main/java/com/vulcan/vmlci/orca/calculator/CalculatorConfig.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright Vulcan Inc. 2021
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.vulcan.vmlci.orca.calculator;
+
+import com.cedarsoftware.util.io.JsonObject;
+import com.cedarsoftware.util.io.JsonReader;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CalculatorConfig extends HashMap<String, CalculatorConfigItem> {
+  private final String config_path;
+
+  public CalculatorConfig(String config_path) throws FileNotFoundException {
+    this.config_path = config_path;
+    FileInputStream reader = new FileInputStream(config_path);
+    Map args = new HashMap<>();
+    args.put(JsonReader.USE_MAPS, true);
+    Object[] loaded_items = (Object[]) JsonReader.jsonToJava(reader, args);
+    for (Object raw_item : loaded_items) {
+      final JsonObject<String, Object> json_item = (JsonObject<String, Object>) raw_item;
+      final String target = (String) json_item.get("target");
+      Object[] parameters = new Object[((Object[]) json_item.get("parameters")).length];
+      for (int i = ((Object[]) json_item.get("parameters")).length - 1; i >= 0; i--) {
+        final Object temp =((Object[]) json_item.get("parameters"))[i];
+        parameters[i] = temp.getClass().cast(temp);
+      }
+      final String function = (String) json_item.get("function");
+      final CalculatorConfigItem item = new CalculatorConfigItem(target, parameters, function);
+      this.put(((CalculatorConfigItem) item).target, (CalculatorConfigItem) item);
+    }
+    System.out.println(loaded_items);
+  }
+
+  public static void main(String[] args) {
+    final String config_path =
+        "/Users/palbee/Development/AMPT/measurement-tool-config/MeasurementConf.json";
+    try {
+      CalculatorConfig instance = new CalculatorConfig(config_path);
+      System.out.println(instance);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
+}
