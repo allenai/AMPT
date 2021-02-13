cad01e35e00fa50a6fa197bc604f9b32721b1b33
Paul Albee <palbee@vulcan.com>
2021-02-12 21:32:25 -0800

    Split CommentInputPanel into standalone class.

---
+++ src/main/java/com/vulcan/vmlci/orca/ui/CommentInputPanel.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright Vulcan Inc. 2021
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.vulcan.vmlci.orca.ui;
+
+import com.vulcan.vmlci.orca.DataStore;
+
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+
+public class CommentInputPanel extends InputPanel {
+  private JTextArea commentField;
+  private JButton saveComments;
+
+  public CommentInputPanel(DataStore dataStore) {
+    super(dataStore);
+  }
+
+  protected void buildUI() {
+    this.setLayout(new BorderLayout(0, 0));
+    commentField = new JTextArea();
+    this.add(commentField, BorderLayout.CENTER);
+    controls.add(commentField);
+    saveComments = new JButton();
+    saveComments.setText("Save Comments");
+    controls.add(saveComments);
+    this.add(saveComments, BorderLayout.SOUTH);
+  }
+
+  protected void wireUI() {}
+}
