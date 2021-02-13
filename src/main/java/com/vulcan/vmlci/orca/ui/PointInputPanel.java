ef0c183b6d06c918069cc353a1b8789934c4a013
Paul Albee <palbee@vulcan.com>
2021-02-12 21:33:17 -0800

    Finished wiring measurements. Overlay functionality not done yet.

--- src/main/java/com/vulcan/vmlci/orca/ui/PointInputPanel.java
+++ src/main/java/com/vulcan/vmlci/orca/ui/PointInputPanel.java
@@ -31,24 +31,29 @@
 
 package com.vulcan.vmlci.orca.ui;
 
-import com.vulcan.vmlci.orca.ColumnDescriptor;
 import com.vulcan.vmlci.orca.DataStore;
 import com.vulcan.vmlci.orca.LastActiveImage;
 import com.vulcan.vmlci.orca.event.ActiveImageChangeEvent;
-import com.vulcan.vmlci.orca.event.ActiveImageListener;
+import ij.ImagePlus;
+import ij.gui.PointRoi;
+import ij.gui.Roi;
+import ij.gui.RoiListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
-import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.event.TableModelEvent;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Rectangle;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.geom.Point2D;
+import java.util.Comparator;
 import java.util.Vector;
 import java.util.stream.Collectors;
 
@@ -64,22 +69,26 @@ public class PointInputPanel extends InputPanel implements RoiListener, ItemList
   private JButton revert;
   private JButton clear;
   private JCheckBox enableOverlays;
-  private ArrayList<JComponent> controls;
 
+  // State Elements
+  private Point2D.Double currentPosition = null;
+  private Point2D.Double savedPosition = null;
 
   public PointInputPanel(DataStore dataStore) {
     super(dataStore);
     PointRoi.addRoiListener(this);
   }
 
-  private void buildUI() {
-    List<ColumnDescriptor> descriptors =
-            dataStore.descriptors.values().stream()
-                    .filter(s -> s.measurement_type.equals("point") && s.name.endsWith("_x"))
-                    .collect(Collectors.toList());
-    descriptors.sort((o1, o2) -> Integer.compare(o1.index, o2.index));
-    Vector<String> measurements = new Vector<>();
-    descriptors.forEach(e -> measurements.add(e.name.substring(0,e.name.length()-2)));
+  protected void buildUI() {
+    Vector<String> measurements =
+        dataStore.descriptors.values().stream() // Give me a stream of descriptors
+            .filter( // Grab the relevant descriptors
+                s ->
+                    s.measurement_type.equals("point") // We need points
+                        && s.name.endsWith("_x")) // but only the x coordinate
+            .sorted(Comparator.comparingInt(o -> o.index)) // Sort, using index field
+            .map(s -> s.name.substring(0, s.name.length() - 2)) // Get part of the descriptor name
+            .collect(Collectors.toCollection(Vector::new)); // Make a vector for JComboBox
 
     this.setLayout(new GridBagLayout());
     final JPanel spacer1 = new JPanel();
@@ -104,7 +113,7 @@ public class PointInputPanel extends InputPanel implements RoiListener, ItemList
     gbc.gridy = 4;
     gbc.fill = GridBagConstraints.HORIZONTAL;
     this.add(currentPointX, gbc);
-    measurementSelector = new JComboBox<String>(measurements);
+    measurementSelector = new JComboBox<>(measurements);
     controls.add(measurementSelector);
     gbc = new GridBagConstraints();
     gbc.gridx = 1;
@@ -241,6 +250,65 @@ public class PointInputPanel extends InputPanel implements RoiListener, ItemList
     gbc.gridwidth = 9;
     this.add(enableOverlays, gbc);
   }
+
+  @Override
+  protected void wireUI() {
+    save.addActionListener(e -> save_point());
+    revert.addActionListener(e -> revert_point());
+    clear.addActionListener(e -> clear_point());
+    measurementSelector.addItemListener(this);
+  }
+
+  private void save_point() {
+    dataStore.set_point(
+        lastActiveImage.getMostRecentImageName(),
+        (String) measurementSelector.getSelectedItem(),
+        currentPosition);
+    savedPosition =
+        dataStore.get_point(
+            lastActiveImage.getMostRecentImageName(),
+            (String) measurementSelector.getSelectedItem());
+    updateInterface();
+  }
+
+  private void revert_point() {
+    if (savedPosition != null) {
+      lastActiveImage.getMostRecentImageWindow().deleteRoi();
+      currentPosition = (Point2D.Double) savedPosition.clone();
+      updateInterface();
+    }
+  }
+
+  private void clear_point() {
+    currentPosition = null;
+    lastActiveImage.getMostRecentImageWindow().deleteRoi();
+    updateInterface();
+  }
+
+  private void updateInterface() {
+    if (savedPosition != null) {
+      savedPointX.setText(Integer.toString((int) savedPosition.x));
+      savedPointY.setText(Integer.toString((int) savedPosition.y));
+    } else {
+      savedPointX.setText("");
+      savedPointY.setText("");
+    }
+
+    if (currentPosition != null) {
+      currentPointX.setText(Integer.toString((int) currentPosition.x));
+      currentPointY.setText(Integer.toString((int) currentPosition.y));
+    } else {
+      currentPointX.setText("");
+      currentPointY.setText("");
+    }
+
+    ImagePlus img = lastActiveImage.getMostRecentImageWindow();
+    Roi active_roi = img.getRoi();
+    if (active_roi == null && currentPosition != null) {
+      img.setRoi(new PointRoi(currentPosition.x, currentPosition.y));
+    }
+  }
+
   /**
    * Gives notification that an ImagePlus has taken focus.
    *
@@ -248,7 +316,73 @@ public class PointInputPanel extends InputPanel implements RoiListener, ItemList
    */
   @Override
   public void activeImageChanged(ActiveImageChangeEvent evt) {
-    boolean new_state = !evt.getNewImage().equals(LastActiveImage.NO_OPEN_IMAGE);
-    controls.forEach(component -> component.setEnabled(new_state));
+    super.activeImageChanged(evt);
+    if (lastActiveImage.getMostRecentImageName() != LastActiveImage.NO_OPEN_IMAGE) {
+      updateInterface();
+    } else {
+      currentPointX.setText("");
+      currentPointY.setText("");
+      savedPointX.setText("");
+      savedPointY.setText("");
+    }
+  }
+
+  /**
+   * This fine grain notification tells listeners the exact range of cells, rows, or columns that
+   * changed.
+   *
+   * @param e
+   */
+  @Override
+  public void tableChanged(TableModelEvent e) {
+    if (e == null) {
+      savedPosition = null;
+    }
+  }
+
+  @Override
+  public void itemStateChanged(ItemEvent e) {
+    if (e.getStateChange() == ItemEvent.SELECTED) {
+      lastActiveImage.getMostRecentImageWindow().deleteRoi();
+      savedPosition =
+          dataStore.get_point(lastActiveImage.getMostRecentImageName(), (String) e.getItem());
+      if (savedPosition != null) {
+        currentPosition = (Point2D.Double) savedPosition.clone();
+      } else {
+        currentPosition = null;
+      }
+      updateInterface();
+    }
+  }
+
+  private void renderLandmark() {
+    ImagePlus img = lastActiveImage.getMostRecentImageWindow();
+    if (currentPointX.getText().isEmpty() || currentPointY.getText().isEmpty()) {
+      img.deleteRoi();
+    } else {
+      img.setRoi(
+          new PointRoi(
+              Double.parseDouble(currentPointX.getText()),
+              Double.parseDouble(currentPointY.getText())));
+    }
+  }
+
+  @Override
+  public void roiModified(ImagePlus imp, int id) {
+    if (imp == null || !imp.getTitle().equals(lastActiveImage.getMostRecentImageName())) {
+      return;
+    }
+
+    if (id == RoiListener.DELETED) {
+      currentPosition = null;
+    } else {
+      Roi roi = imp.getRoi();
+      Rectangle bounds = roi.getBounds();
+      if (currentPosition == null) {
+        currentPosition = new Point2D.Double();
+      }
+      currentPosition.setLocation(bounds.x, bounds.y);
+    }
+    updateInterface();
   }
 }
