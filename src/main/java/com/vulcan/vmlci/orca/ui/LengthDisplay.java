e8618799394212791a385ea5467f81cbf4e03210
Paul Albee <palbee@vulcan.com>
2021-02-22 22:17:45 -0800

    Initial length display panel

---
+++ src/main/java/com/vulcan/vmlci/orca/ui/LengthDisplay.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright Vulcan Inc. 2021
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.vulcan.vmlci.orca.ui;
+
+import com.vulcan.vmlci.orca.data.ColumnDescriptor;
+import com.vulcan.vmlci.orca.data.DataStore;
+import com.vulcan.vmlci.orca.helpers.MeasurementTableModel;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableModel;
+import java.awt.Color;
+import java.util.function.Predicate;
+
+public class LengthDisplay extends JPanel {
+  public LengthDisplay(DataStore dataStore, Predicate<ColumnDescriptor> selection_filter) {
+    build_ui(new MeasurementTableModel(dataStore, selection_filter));
+  }
+
+  private void build_ui(TableModel myModel) {
+    JTable table = new JTable(myModel);
+//    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+    table.setShowGrid(true);
+    table.setGridColor(Color.BLACK);
+    table.doLayout();
+    JScrollPane scrollPane = new JScrollPane(table);
+    scrollPane.doLayout();
+    this.add(scrollPane);
+  }
+}
