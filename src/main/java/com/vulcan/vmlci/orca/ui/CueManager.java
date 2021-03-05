b3febadce24a8385e714966b3bf7861ef92bce25
Paul Albee <palbee@vulcan.com>
2021-03-04 21:15:24 -0800

    Refactored the drawing methods to hide most of the complexity inside of the CueManager.

--- src/main/java/com/vulcan/vmlci/orca/ui/CueManager.java
+++ src/main/java/com/vulcan/vmlci/orca/ui/CueManager.java
@@ -41,29 +41,51 @@ import ij.ImagePlus;
 import ij.gui.Line;
 import ij.gui.Overlay;
 import ij.gui.PointRoi;
+import ij.gui.Roi;
 
 import javax.swing.JToggleButton;
 import java.io.FileNotFoundException;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Vector;
 
 public class CueManager {
+  public JToggleButton.ToggleButtonModel cueToggle;
+  public JToggleButton.ToggleButtonModel overlayToggle;
   private DataStore dataStore;
   private LastActiveImage lastActiveImage;
   private String config_file = "CueConfig.json";
   private HashMap<String, Vector<String>> cue_lookup;
   private ReferenceCalculator referenceCalculator;
-  public JToggleButton.ToggleButtonModel toggleButtonModel;
+  /** The condition line selected for rendering */
+  private HashSet<String> conditionLines;
+  /** The measurement cue selected for rendering */
+  private String activeCue;
 
   public CueManager(DataStore dataStore)
       throws FileNotFoundException, ConfigurationFileLoadException {
     this.dataStore = dataStore;
     this.lastActiveImage = LastActiveImage.getInstance();
-    this.toggleButtonModel = new JToggleButton.ToggleButtonModel();
+    this.cueToggle = new JToggleButton.ToggleButtonModel();
+    this.overlayToggle = new JToggleButton.ToggleButtonModel();
     cue_lookup = new HashMap<>();
-
+    conditionLines = new HashSet<>();
     referenceCalculator = new ReferenceCalculator(dataStore);
     load_configuration();
+
+    // Configure toggles (essentially a radio group at allows de-selection).
+    cueToggle.addActionListener(
+        e -> {
+          if (cueToggle.isSelected()) {
+            overlayToggle.setSelected(false);
+          }
+        });
+    overlayToggle.addActionListener(
+        e -> {
+          if (overlayToggle.isSelected()) {
+            cueToggle.setSelected(false);
+          }
+        });
   }
 
   private void load_configuration() throws ConfigurationFileLoadException {
@@ -77,26 +99,66 @@ public class CueManager {
   }
 
   /**
-   * Draws cue lines on the image to aid measurement.
+   * Returns the cue that cue manager will draw.
    *
-   * @param measurement The current active measurement
+   * @return the currently active cue
    */
-  public void draw_cue(String measurement) {
-    if (lastActiveImage.getMostRecentImageName().equals(LastActiveImage.NO_OPEN_IMAGE)) {
+  public String getActiveCue() {
+    return activeCue;
+  }
+
+  /**
+   * Select the next visible cue.
+   *
+   * @param activeCue the cue to draw when appropriate.
+   */
+  public void setActiveCue(String activeCue) {
+    this.activeCue = activeCue;
+  }
+
+  public boolean getConditionLine(String condition_line) {
+    return conditionLines.contains(condition_line);
+  }
+
+  public void setConditionLine(String conditionLine, boolean draw) {
+    if (draw) {
+      conditionLines.add(conditionLine);
+    } else {
+      conditionLines.remove(conditionLine);
+    }
+  }
+
+  /** Update the cue/condition line overaly. */
+  public void draw() {
+    if (lastActiveImage.no_images()) {
       return;
     }
     String image_name = lastActiveImage.getMostRecentImageName();
-    ImagePlus image = lastActiveImage.getMostRecentImageWindow();
-    image.setOverlay(null);
+    ImagePlus img = lastActiveImage.getMostRecentImageWindow();
 
-    if (!cue_lookup.containsKey(measurement)) {
-      return;
+    if (cueToggle.isSelected()) {
+      img.setOverlay(draw_cue(image_name));
+    } else if (overlayToggle.isSelected()) {
+      img.setOverlay(drawOverlays(image_name));
+    } else {
+      img.setOverlay(null);
+    }
+  }
+
+  /**
+   * Draws cue lines on the image to aid measurement.
+   *
+   * @param image_name The image being drawn on.
+   */
+  private Overlay draw_cue(String image_name) {
+    if (!cue_lookup.containsKey(activeCue)) {
+      return null;
     }
     Overlay overlay = new Overlay();
     overlay.drawNames(true);
     overlay.drawLabels(true);
     overlay.setLabelFontSize(16, "");
-    for (String cue : cue_lookup.get(measurement)) {
+    for (String cue : cue_lookup.get(activeCue)) {
       HashMap<String, Point[]> guideline;
       guideline = (HashMap<String, Point[]>) referenceCalculator.do_measurement(cue, image_name);
       for (String label : guideline.keySet()) {
@@ -104,17 +166,37 @@ public class CueManager {
         if (label.equals("axis")) {
           label = "";
         }
+        Roi marker = null;
         switch (endpoints.length) {
           case 1:
-            overlay.add(new PointRoi(endpoints[0].x, endpoints[0].y), label);
+            marker = new PointRoi(endpoints[0].x, endpoints[0].y);
             break;
           case 2:
-            overlay.add(
-                new Line(endpoints[0].x, endpoints[0].y, endpoints[1].x, endpoints[1].y), label);
+            marker = new Line(endpoints[0].x, endpoints[0].y, endpoints[1].x, endpoints[1].y);
             break;
+        }
+        if (marker != null) {
+          marker.setName(label);
+          overlay.add(marker, label);
         }
       }
     }
-    image.setOverlay(overlay);
+    return overlay;
+  }
+
+  /** Draws the condition lines specified via addConditionLine amd removeConditionLine */
+  public Overlay drawOverlays(String image_name) {
+    if (conditionLines.isEmpty()) {
+      return null;
+    }
+    Overlay overlay = new Overlay();
+    for (String conditionLine : conditionLines) {
+      HashMap<String, Point[]> guideline;
+      Point[] endpoints = dataStore.getEndpoints(image_name, conditionLine);
+      if (endpoints != null) {
+        overlay.add(new Line(endpoints[0].x, endpoints[0].y, endpoints[1].x, endpoints[1].y));
+      }
+    }
+    return overlay;
   }
 }
