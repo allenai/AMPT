f67858f6105f1f55b3e26614d5d744c008e7c03e
Paul Albee <palbee@vulcan.com>
2021-03-17 21:54:23 -0700

    Cleaned up code.

--- src/main/java/com/vulcan/vmlci/orca/ui/Branding.java
+++ src/main/java/com/vulcan/vmlci/orca/ui/Branding.java
@@ -31,39 +31,39 @@
 
 package com.vulcan.vmlci.orca.ui;
 
-import com.vulcan.vmlci.orca.ui.AccordionPanel;
-
-import javax.swing.*;
-import java.awt.*;
-
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 /**
- * The <code>Branding</code> class is part of the UI that identifies the plug and manages the about an
- * help menus.
+ * The <code>Branding</code> class is part of the UI that identifies the plug and manages the about
+ * an help menus.
  */
 public class Branding extends JPanel {
 
-    JLabel title;
+  final JLabel title;
 
-    public Branding() {
-        super();
+  public Branding() {
+      GridBagConstraints gbc;
+      setLayout(new GridBagLayout());
 
-        GridBagConstraints gbc;
-        this.setLayout(new GridBagLayout());
+    title = new JLabel("Aquatic Mammal Photogrammetry Tool");
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.weightx = 1.0;
+    gbc.anchor = GridBagConstraints.WEST;
+      add(title, gbc);
 
-        title = new JLabel("Aquatic Mammal Photogrammetry Tool");
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.weightx = 1.0;
-        gbc.anchor = GridBagConstraints.WEST;
-        this.add(title, gbc);
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.EAST;
-        final Icon hamburger = new ImageIcon(AccordionPanel.class.getResource("/images/hamburger_32.png"));
-        this.add(new JLabel(hamburger), gbc);
-    }
+    gbc = new GridBagConstraints();
+    gbc.gridx = 1;
+    gbc.gridy = 0;
+    gbc.anchor = GridBagConstraints.EAST;
+    final Icon hamburger =
+        new ImageIcon(AccordionPanel.class.getResource("/images/hamburger_32.png"));
+      add(new JLabel(hamburger), gbc);
+  }
 }
