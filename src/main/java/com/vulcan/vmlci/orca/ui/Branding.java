5a834962b207d9a0c9f874e18c1c1b61f6c6f379
Paul Albee <palbee@vulcan.com>
2021-03-19 15:20:04 -0700

    Initial population of hamburger menu.

--- src/main/java/com/vulcan/vmlci/orca/ui/Branding.java
+++ src/main/java/com/vulcan/vmlci/orca/ui/Branding.java
@@ -33,8 +33,15 @@ package com.vulcan.vmlci.orca.ui;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
@@ -44,19 +51,25 @@ import java.awt.GridBagLayout;
  */
 public class Branding extends JPanel {
 
-  final JLabel title;
+  JLabel title;
+  JPopupMenu menu;
+  JButton burgerButton;
 
   public Branding() {
-      GridBagConstraints gbc;
-      setLayout(new GridBagLayout());
+    buildUI();
+    wireUI();
+  }
 
+  private void buildUI() {
+    GridBagConstraints gbc;
+    setLayout(new GridBagLayout());
     title = new JLabel("Aquatic Mammal Photogrammetry Tool");
     gbc = new GridBagConstraints();
     gbc.gridx = 0;
     gbc.gridy = 0;
     gbc.weightx = 1.0;
     gbc.anchor = GridBagConstraints.WEST;
-      add(title, gbc);
+    add(title, gbc);
 
     gbc = new GridBagConstraints();
     gbc.gridx = 1;
@@ -64,6 +77,33 @@ public class Branding extends JPanel {
     gbc.anchor = GridBagConstraints.EAST;
     final Icon hamburger =
         new ImageIcon(AccordionPanel.class.getResource("/images/hamburger_32.png"));
-      add(new JLabel(hamburger), gbc);
+    burgerButton = new JButton(hamburger);
+    burgerButton.setComponentPopupMenu(buildMenu());
+    add(burgerButton, gbc);
+  }
+
+  private void wireUI() {
+    burgerButton.addActionListener(
+        e -> {
+          menu.show(burgerButton, burgerButton.getWidth() / 2, burgerButton.getHeight() / 2);
+        });
+  }
+
+  private JPopupMenu buildMenu() {
+    menu = new JPopupMenu();
+    JMenuItem menuItem = new JMenuItem("About");
+    menuItem.addActionListener(
+        e -> {
+          About dialog = new About((Frame) this.getTopLevelAncestor());
+          dialog.setLocationByPlatform(true);
+          dialog.setLocationRelativeTo(this.getTopLevelAncestor());
+          dialog.setVisible(true);
+        });
+    menu.add(menuItem);
+    menuItem = new JMenuItem("User Guide");
+    menu.add(menuItem);
+    menuItem = new JMenuItem("Preferences");
+    menu.add(menuItem);
+    return menu;
   }
 }
