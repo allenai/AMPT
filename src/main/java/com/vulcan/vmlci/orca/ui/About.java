b81af297353321d5a14bbeb436492c47c0279d19
Paul Albee <palbee@vulcan.com>
2021-03-19 15:19:12 -0700

    First pass on About dialog.

---
+++ src/main/java/com/vulcan/vmlci/orca/ui/About.java
@@ -0,0 +1,215 @@
+/*
+ * Copyright Vulcan Inc. 2021
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.vulcan.vmlci.orca.ui;
+
+import org.scijava.log.Logger;
+import org.scijava.log.StderrLogService;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.KeyStroke;
+import javax.swing.WindowConstants;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+public class About extends JDialog {
+  private static final String COPYRIGHT_2021_VULCAN_INC = "Copyright 2021 Vulcan Inc.";
+  final Logger logger = new StderrLogService();
+  private JPanel contentPane;
+  private JButton buttonDismiss;
+  private JTextPane licensePanel;
+
+  public About(){
+    this(null);
+  }
+  public About(Frame owner){
+    super(owner);
+    buildUI();
+    setContentPane(contentPane);
+    setModal(true);
+    getRootPane().setDefaultButton(buttonDismiss);
+    pack();
+    setSize(new Dimension(640, 480));
+    wireUI();
+  }
+
+  private void buildUI() {
+    contentPane = new JPanel();
+    contentPane.setLayout(new GridBagLayout());
+    final JPanel panel1 = new JPanel();
+    panel1.setLayout(new GridBagLayout());
+    GridBagConstraints gbc;
+    gbc = new GridBagConstraints();
+    gbc.gridx = 1;
+    gbc.gridy = 2;
+    gbc.weightx = 1.0;
+    gbc.fill = GridBagConstraints.BOTH;
+    contentPane.add(panel1, gbc);
+    final JPanel panel2 = new JPanel();
+    panel2.setLayout(new GridBagLayout());
+    gbc = new GridBagConstraints();
+    gbc.gridx = 2;
+    gbc.gridy = 0;
+    gbc.fill = GridBagConstraints.BOTH;
+    panel1.add(panel2, gbc);
+    buttonDismiss = new JButton();
+    buttonDismiss.setText("Dismiss");
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.weightx = 1.0;
+    gbc.weighty = 1.0;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    panel2.add(buttonDismiss, gbc);
+    final JPanel spacer1 = new JPanel();
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.weightx = 1.0;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    panel1.add(spacer1, gbc);
+    final JPanel panel3 = new JPanel();
+    panel3.setLayout(new GridBagLayout());
+    gbc = new GridBagConstraints();
+    gbc.gridx = 1;
+    gbc.gridy = 1;
+    gbc.weightx = 1.0;
+    gbc.weighty = 1.0;
+    gbc.fill = GridBagConstraints.BOTH;
+    gbc.ipadx = 1;
+    gbc.ipady = 1;
+    contentPane.add(panel3, gbc);
+    final JScrollPane scrollPane1 = new JScrollPane();
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 0;
+    gbc.weightx = 1.0;
+    gbc.weighty = 1.0;
+    gbc.fill = GridBagConstraints.BOTH;
+    panel3.add(scrollPane1, gbc);
+    licensePanel = new JTextPane();
+    licensePanel.setContentType("text/html");
+    licensePanel.setEditable(false);
+    licensePanel.setEnabled(true);
+    licensePanel.setText(getLicenseText());
+    licensePanel.putClientProperty("html.disable", Boolean.FALSE);
+    scrollPane1.setViewportView(licensePanel);
+    final JPanel spacer2 = new JPanel();
+    gbc = new GridBagConstraints();
+    gbc.gridx = 1;
+    gbc.gridy = 0;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    panel3.add(spacer2, gbc);
+    final JPanel spacer3 = new JPanel();
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    gbc.fill = GridBagConstraints.VERTICAL;
+    panel3.add(spacer3, gbc);
+    final JPanel spacer4 = new JPanel();
+    gbc = new GridBagConstraints();
+    gbc.gridx = 0;
+    gbc.gridy = 1;
+    gbc.fill = GridBagConstraints.HORIZONTAL;
+    contentPane.add(spacer4, gbc);
+    final JPanel spacer5 = new JPanel();
+    gbc = new GridBagConstraints();
+    gbc.gridx = 1;
+    gbc.gridy = 0;
+    gbc.fill = GridBagConstraints.VERTICAL;
+    contentPane.add(spacer5, gbc);
+  }
+
+  private void wireUI() {
+    buttonDismiss.addActionListener(e -> onClose());
+
+    // call onClose() when cross is clicked
+    setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+    addWindowListener(new AboutWindowAdapter());
+
+    // call onClose() on ESCAPE
+    contentPane.registerKeyboardAction(
+        new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            onClose();
+          }
+        },
+        KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+        JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+  }
+
+  private String getLicenseText() {
+    String license = About.COPYRIGHT_2021_VULCAN_INC;
+    ;
+    ClassLoader classLoader = getClass().getClassLoader();
+    final InputStream licenseInput = classLoader.getResourceAsStream("license.html");
+    if (null != licenseInput) {
+      try (final BufferedReader licenseReader =
+          new BufferedReader(new InputStreamReader(licenseInput))) {
+        license = licenseReader.lines().collect(Collectors.joining());
+        licenseInput.close();
+      } catch (IOException e) {
+        logger.error(e);
+      }
+    } else {
+      logger.error("Could not find license.html in resource bundle.");
+    }
+    return license;
+  }
+
+  private void onClose() {
+    // add your code here
+    dispose();
+  }
+  private class AboutWindowAdapter extends WindowAdapter {
+    public void windowClosing(WindowEvent e) {
+      onClose();
+    }
+  }
+}
