c4b2b6ba57362897e5a281bede0f7f8fe9ff3615
Patrick Johnson <PatrickJ@vulcan.com>
2021-06-07 15:26:23 -0700

    Remove now unused Date import.

--- src/test/java/com/vulcan/vmlci/orca/data/DataStoreTest.java
+++ src/test/java/com/vulcan/vmlci/orca/data/DataStoreTest.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Date;
 
 public class DataStoreTest extends TestCase {
   final String[] SAMPLE_SHORT_FILES = {
